5d06ee2a6f99a350a8797947d67ef6ad6e118d6b
Kent Vugs Nielsen <19304326+kent1325@users.noreply.github.com>
2023-09-21 16:33:38 +0200

    added mlflow

---
+++ src/utils/utils.py
@@ -0,0 +1,27 @@
+import mlflow
+from sklearn.metrics import (
+    f1_score,
+    accuracy_score,
+    precision_score,
+    recall_score,
+    roc_auc_score,
+    confusion_matrix,
+)
+
+
+def calculate_metrics(y_pred, y_true, epoch, type):
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+    accuracy = accuracy_score(y_true, y_pred)
+    mlflow.log_metric(key=f"F1-Score-{type}", value=float(f1), step=epoch)
+    mlflow.log_metric(key=f"Accuracy-{type}", value=float(accuracy), step=epoch)
+    mlflow.log_metric(key=f"Precision-{type}", value=float(precision), step=epoch)
+    mlflow.log_metric(key=f"Recall-{type}", value=float(recall), step=epoch)
+    try:
+        roc = roc_auc_score(y_true, y_pred)
+        print(f"ROC AUC: {roc}")
+        mlflow.log_metric(key=f"ROC-AUC-{type}", value=float(roc), step=epoch)
+    except:
+        mlflow.log_metric(key=f"ROC-AUC-{type}", value=float(0), step=epoch)
+        print(f"ROC AUC: notdefined")
