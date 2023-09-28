cc4f5e40ad829648701ec89f5df38ca5974605d0
Kowalski2332 <frederik_kowalski@hotmail.com>
2023-09-28 23:41:23 +0200

    Added example of optuna plot

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import optuna
+from optuna.visualization import plot_optimization_history
 from data.get_dataloader import MUTAGLoader
 from networks.gnn_loader import GAT, GCN
 from dotenv import load_dotenv
@@ -24,14 +25,17 @@ if __name__ == "__main__":
     # print(model)
     # print(f"Number of parameters: {count_parameters(model)}")
 
-    study = optuna.create_study(direction="maximize")
+    study = optuna.create_study(
+        direction="maximize"
+    )
     study.optimize(
         lambda trial: objective_cv(
             trial=trial, model=model, train_dataset=train_dataset
         ),
-        n_trials=1,
+        n_trials=5,
         timeout=600,
     )
+    plot_optimization_history(study).show()
 
     pruned_trials = [
         t for t in study.trials if t.state == optuna.trial.TrialState.PRUNED
@@ -39,17 +43,15 @@ if __name__ == "__main__":
     complete_trials = [
         t for t in study.trials if t.state == optuna.trial.TrialState.COMPLETE
     ]
-
+    
     print("Study statistics: ")
     print("  Number of finished trials: ", len(study.trials))
     print("  Number of pruned trials: ", len(pruned_trials))
     print("  Number of complete trials: ", len(complete_trials))
 
-    print("Best trial:")
     trial = study.best_trial
-
+    print("Best trial:", trial.number)
     print("  Value: ", trial.value)
-
     print("  Params: ")
     for key, value in trial.params.items():
         print("    {}: {}".format(key, value))
