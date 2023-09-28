bae1ecf607ab791d97bdc5f4ac45b8a3fd7e2017
Kowalski2332 <frederik_kowalski@hotmail.com>
2023-09-28 23:42:56 +0200

    Edited prepare for plotting function

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -14,6 +14,19 @@ from settings.config import (
     K_FOLDS,
 )
 
+# Dict to use in combination with prepare for plotting function in training and testing phase
+metric_arrays = {
+    "Train Accuracy": np.zeros((EPOCHS - 1)),
+    "Train Precision": np.zeros((EPOCHS - 1)),
+    "Train Recall": np.zeros((EPOCHS - 1)),
+    "Train F1": np.zeros((EPOCHS - 1)),
+    "Train Roc": np.zeros((EPOCHS - 1)),
+    "Test Accuracy": np.zeros((EPOCHS - 1)),
+    "Test Precision": np.zeros((EPOCHS - 1)),
+    "Test Recall": np.zeros((EPOCHS - 1)),
+    "Test F1": np.zeros((EPOCHS - 1)),
+    "Test Roc": np.zeros((EPOCHS - 1)),
+}
 
 def calculate_metrics(y_pred, y_true):
     precision = precision_score(y_true, y_pred, zero_division=0)
@@ -23,32 +36,11 @@ def calculate_metrics(y_pred, y_true):
     roc = roc_auc_score(y_true, y_pred)
     return precision, recall, f1, accuracy, roc
 
-def prepare_for_plotting(y_pred, y_true, precision_arr, recall_arr, accuracy_arr, f1_arr, roc_arr, type, fold, epoch):
-    precision, recall, f1, accuracy, roc = calculate_metrics(y_pred, y_true)
+def prepare_for_plotting(metric_storage: dict, metric_name: str, metric_value: float, type: str, epoch: int): 
+    metric_storage[metric_name][(epoch - 1)] = metric_value
     
-    if type == "tuning":
-        precision_arr[fold, (epoch - 1)] = precision
-        recall_arr[fold, (epoch - 1)] = recall
-        f1_arr[fold, (epoch - 1)] = f1
-        accuracy_arr[fold, (epoch - 1)] = accuracy
-        roc_arr[fold, (epoch - 1)] = roc
-        plot_results = [np.mean(precision_arr, axis=0), np.mean(recall_arr, axis=0), np.mean(f1_arr, axis=0), np.mean(accuracy_arr, axis=0), np.mean(roc_arr, axis=0)]
-        plot_labels = ["precision", "recall", "f1", "accuracy", "roc"]
-        
-    elif type == "training":
-        precision_arr[epoch - 1] = precision
-        recall_arr[epoch - 1] = recall
-        f1_arr[epoch - 1] = f1
-        accuracy_arr[epoch - 1] = accuracy
-        roc_arr[epoch - 1] = roc
-        plot_results = precision_arr, recall_arr, f1_arr, accuracy_arr, roc_arr
-        plot_labels = ["precision", "recall", "f1", "accuracy", "roc"]
-    
-    return plot_results, plot_labels
-
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
-
 
 def reset_weights(m: torch.nn.Module):
     """Try resetting model weights to avoid weight leakage.
